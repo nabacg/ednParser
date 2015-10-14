5f388422889547cbe16458804ad50579b33b793f
nabacg <nabacg@gmail.com>
2015-10-14 15:07:11 +0100

    adding mini repl

---
+++ src/main/scala/net/cabworks/EdnParser/EdnRepl.scala
@@ -0,0 +1,30 @@
+package net.cabworks.EdnParser
+import net.cabworks.EdnParser.EdnParser
+
+import scala.io.StdIn
+
+/**
+ * Created by cab on 12/10/2015.
+ */
+object EdnRepl {
+
+  def |>[T, R] (a : T, f : (T) => R) : R = f(a)
+
+  def replLoop : Unit = {
+    print("Edn>")
+    Console.flush()
+    val input = StdIn.readLine()
+    input match {
+      case in : String if in.length() > 0 => {
+        //this |> ( EdnParser.eval(input), println)
+        val result = EdnParser.eval(in)
+        println(result)
+        replLoop
+      }
+      case _ => ()
+    }
+
+  }
+
+  def main(args: Array[String]) = replLoop
+}
